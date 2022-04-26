0913c3ae055391342e79f6fb419ebf8c83051a36
Aaron <rhanfwlq980@naver.com>
2022-04-26 14:15:33 +0900

    add: race condition example without synchronization

---
+++ Ch6-Synchronization_Tools/race_nosync.c
@@ -0,0 +1,19 @@
+/*
+ * Copyright 2022. Heekuck Oh, all rights reserved.
+ * 이 프로그램은 한양대학교 ERICA 소프트웨어학부 재학생을 위해 작성하였다.
+ */
+#include <stdio.h>
+
+int main(void)
+{
+    int x;
+    
+    x = 0;
+    #pragma omp parallel
+    {
+        x = x + 1;
+    }
+    printf("x = %d\n", x);
+    
+    return 0;
+}
